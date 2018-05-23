82d2f0e1915b3cbad5d68eadc137729dac99e6a4
remclay <remmy.lauren@gmail.com>
2018-05-23 15:43:42 +1000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,19 @@
 class RepositoriesController < ApplicationController
+
   def index
+    response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    # resp = Faraday.get("https://github.com/user/repos") do |req|
+    #   req.params['access_token'] = session[:token]
+    #   # req.params['Authorization'] = session[:token]
+    # end
+    @repos = JSON.parse(response.body)
   end
 
   def create
+    resp = Faraday.post("https://github.com/user/repos") do |req|
+      req.params["name"] = params["name"].to_json
+      req.params['access_token'] = session[:token]
+    end
+    redirect_to '/'
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,7 +29,64 @@ RSpec.configure do |config|
       with(:body => {"{\"name\":\"a-new-repo\"}"=>true},
       :headers => {'Authorization'=>'token 1'}).
       to_return(:status => 201, :body => "", :headers => {})
+    #
+    #
+    #
+    #   stub_request(:post, "https://github.com/login/oauth/access_token?client_id=440982e5e9ba1389332c&client_secret=6838442f870b325490d57d4a964cc0884d84c8fb&code=20").
+    #            with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Length'=>'0', 'User-Agent'=>'Faraday v0.9.1'}).
+    #            to_return(:status => 200, :body => "", :headers => {})
+    #
+    #          # registered request stubs:
+    #
+    #          stub_request(:post, "https://api.github.com/user/repos").
+    #            with(:body => {"{\"name\":\"a-new-repo\"}"=>true},
+    #                 :headers => {'Authorization'=>'token 1'})
+    #          stub_request(:get, "https://api.github.com/user").
+    #            with(:headers => {'Authorization'=>'token 1'})
+    #          stub_request(:post, "https://github.com/login/oauth/access_token").
+    #            with(:body => {"client_id"=>"440982e5e9ba1389332c", "client_secret"=>"6838442f870b325490d57d4a964cc0884d84c8fb", "code"=>"20"},
+    #                 :headers => {'Accept'=>'application/json'})
+    #          stub_request(:get, "https://api.github.com/user/repos").
+    #            with(:headers => {'Authorization'=>'token 1'})
+    #
+    #
+    #
+
+      #
+      #
+      # stub_request(:get, "https://github.com/user/repos?access_token=1").
+      #          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Faraday v0.9.1'}).
+      #          to_return(:status => 200, :body => "", :headers => {})
+      #
+      #        # registered request stubs:
+      #
+      #        stub_request(:post, "https://api.github.com/user/repos").
+      #          with(:body => {"{\"name\":\"a-new-repo\"}"=>true},
+      #               :headers => {'Authorization'=>'token 1'})
+      #        stub_request(:get, "https://api.github.com/user").
+      #          with(:headers => {'Authorization'=>'token 1'})
+      #        stub_request(:post, "https://github.com/login/oauth/access_token").
+      #          with(:body => {"client_id"=>"440982e5e9ba1389332c", "client_secret"=>"6838442f870b325490d57d4a964cc0884d84c8fb", "code"=>"20"},
+      #               :headers => {'Accept'=>'application/json'})
+      #        stub_request(:get, "https://api.github.com/user/repos").
+      #          with(:headers => {'Authorization'=>'token 1'})
+      #
+      #
+      #
+      #
+      # stub_request(:post, "https://api.github.com/user/repos").
+      #    with(:body => {"{\"name\":\"a-new-repo\"}"=>true},
+      #         :headers => {'Authorization'=>'token 1'})
+      #  stub_request(:get, "https://api.github.com/user").
+      #    with(:headers => {'Authorization'=>'token 1'})
+      #  stub_request(:post, "https://github.com/login/oauth/access_token").
+      #    with(:body => {"client_id"=>"440982e5e9ba1389332c", "client_secret"=>"6838442f870b325490d57d4a964cc0884d84c8fb", "code"=>"20"},
+      #         :headers => {'Accept'=>'application/json'})
+      #  stub_request(:get, "https://api.github.com/user/repos").
+      #    with(:headers => {'Authorization'=>'token 1'})
+
   end
 end
 
 WebMock.disable_net_connect!(allow_localhost: true)
+# WebMock.allow_net_connect!
